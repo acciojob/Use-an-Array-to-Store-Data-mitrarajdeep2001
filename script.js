f5c93c8fbe1a79d087a024113bce22cc756b7bc1
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-08 08:25:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+let yourArray = [
+	[1, 2 ,3.5, 4, 5.5],
+	["hello", "world"],
+	[true, false],
+	[
+		{
+	      one: 1,
+	      two: 2
+	    },
+	    {
+	      three: 3,
+	      four: 4
+	    }
+	],
+	[null],
+	[undefined]
+]
